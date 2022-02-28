7eddef7dfc6a42e2325d9f2611f27d93e9ed24b7
Davi Gomes <99661607+DaviS1Gomes@users.noreply.github.com>
2022-02-28 00:59:50 -0300

    Add files via upload

---
+++ Consumo.cpp
@@ -0,0 +1,12 @@
+#include<math.h>
+#include<stdio.h>
+
+int main()
+{
+    int valorX;
+    float valorY;
+        scanf("%d %f", &valorX,&valorY);
+        printf("%.3f km/l\n", valorX/valorY);
+
+    return 0;
+}
